0bcbfffc66787bad6d7bd8eb5ea8ad3187692170
markusvilcans <markusvilcans@gmail.com>
2023-04-21 16:56:37 +0300

    AI rigidbody

--- Assets/Scripts/EnemyPatrol.cs
+++ Assets/Scripts/EnemyPatrol.cs
@@ -7,43 +7,46 @@ public class EnemyPatrol : MonoBehaviour
     public Transform[] patrolPoints;
     public int targetPoint;
     public float speed;
-    Vector3 target;
     public float rotationSpeed;
-    // Start is called before the first frame update
+    public float gravity = 20f;
+    private Rigidbody rb;
+    Vector3 target;
+
     void Start()
     {
         targetPoint = 0;
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if(transform.position == patrolPoints[targetPoint].position)
+        if (transform.position == patrolPoints[targetPoint].position)
         {
             increaseTargetInt();
-
         }
+
         if (Vector3.Distance(transform.position, patrolPoints[targetPoint].position) > 0.1f)
         {
-            // Set isWalking parameter to true
             GetComponent<Animator>().SetBool("isWalking", true);
         }
         else
         {
-            // Set isWalking parameter to false
             GetComponent<Animator>().SetBool("isWalking", false);
         }
-            Vector3 direction = patrolPoints[targetPoint].position - transform.position;
-            Quaternion targetRotation = Quaternion.LookRotation(direction);
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, Time.deltaTime * rotationSpeed);
-            transform.position = Vector3.MoveTowards(transform.position, patrolPoints[targetPoint].position, speed * Time.deltaTime);
-    }
 
+        Vector3 direction = patrolPoints[targetPoint].position - transform.position;
+        Quaternion targetRotation = Quaternion.LookRotation(direction);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, Time.deltaTime * rotationSpeed);
+        transform.position = Vector3.MoveTowards(transform.position, patrolPoints[targetPoint].position, speed * Time.deltaTime);
+
+        rb.AddForce(Vector3.down * gravity);
+    }
 
     void increaseTargetInt()
     {
         targetPoint++;
-        if(targetPoint >= patrolPoints.Length){
+        if (targetPoint >= patrolPoints.Length)
+        {
             targetPoint = 0;
         }
     }
